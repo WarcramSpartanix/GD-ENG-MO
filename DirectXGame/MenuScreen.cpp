61b0e3b5f082f3ee222b2a04fb547afc6f69146c
JasoSantiago <68273034+JasoSantiago@users.noreply.github.com>
2021-08-24 12:41:25 +0800

    menuscreen drawUI Change

--- DirectXGame/MenuScreen.cpp
+++ DirectXGame/MenuScreen.cpp
@@ -1,4 +1,11 @@
 #include "MenuScreen.h"
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_dx11.h"
+#include "ImGui/imgui_impl_win32.h"
+#include <iostream>
+#include "GraphicsEngine.h"
+#include "VertexShader.h"
+#include "UIManager.h"
 
 MenuScreen::MenuScreen() : AUIScreen("Menu")
 {
@@ -10,4 +17,27 @@ MenuScreen::~MenuScreen()
 
 void MenuScreen::drawUI()
 {
+	if(ImGui::BeginMainMenuBar()) {
+		if (ImGui::BeginMenu("File")) {
+			if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
+			if (ImGui::MenuItem("Save", "Ctrl+S")) { /* Do stuff */ }
+			if (ImGui::MenuItem("Save As...", "Ctrl+Shift+S")) { /* Do stuff */ }
+			if (ImGui::MenuItem("Exit Editor", "Ctrl+W")) {/*Do something */ }
+			ImGui::EndMenu();
+		}
+		if (ImGui::BeginMenu("Game Object")) {
+			if (ImGui::MenuItem("Create Camera"))
+			if (ImGui::BeginMenu("Light")) {
+				if (ImGui::MenuItem("Point Light")) { /* Do stuff */ }
+				ImGui::EndMenu();
+			}
+		}
+		if(ImGui::BeginMenu("Start Game"))
+		{
+			if (ImGui::MenuItem("Play"))
+				if (ImGui::MenuItem("Stop"))
+					ImGui::EndMenu();
+		}
+		ImGui::EndMainMenuBar();
+	}
 }
